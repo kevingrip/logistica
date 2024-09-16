761f11021192aaa6559a82019e5b49a61c32a020
Kevin Grippo <grippokevin@gmail.com>
2024-09-16 13:12:20 -0300

    act

--- preciosAct.py
+++ preciosAct.py
@@ -6,16 +6,16 @@ with open("zone.json","r") as zjs:
 for zones in zone:
     for item in zone[zones]:
         if (item["LOCALIDAD"] in ("San Fernando","Ituzaingo","La Matanza Norte","Malvinas Argentinas","San Isidro","San Miguel","Vicente Lopez")):
-            item["PRECIO"]=5105
+            item["PRECIO"]=6024
             item["CORDON"]=2
         if (item["LOCALIDAD"] in ("Almirante Brown","Florencio Varela","Berazategui")):
-            item["PRECIO"]=6975        
+            item["PRECIO"]=8300        
             item["CORDON"]=3
         if (item["LOCALIDAD"] in ("Hurlingham","Moron","San Martin","Tres De Febrero")):
             item["CORDON"]=1
-            item["PRECIO"]=3235
+            item["PRECIO"]=3785
         else:
-            if item["PRECIO"]==8300:
+            if item["PRECIO"]==8500:
                 item["CORDON"]=4
 
 
